69f968c615791688cb0c8645515e1b9c95cb5e09
Elivelton <eliveltonmn@outlook.com>
2022-10-19 17:46:55 -0300

    Terceiro requisito

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,17 +2,68 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Filter() {
-  const { nameFilter, handleFilterByName } = useContext(PlanetsContext);
+  const {
+    nameFilter,
+    handleFilterByName,
+    colunsFilter,
+    handleFilterByColuns,
+    operatorFilter,
+    handleFilterByOperator,
+    numberFilter,
+    handleFilterByNumber,
+    handleFilter,
+  } = useContext(PlanetsContext);
+
   return (
     <div>
       <input
         data-testid="name-filter"
         type="text"
-        name="name"
+        name="nameFilter"
         value={ nameFilter }
         placeholder="Digite um nome"
         onChange={ handleFilterByName }
       />
+
+      <select
+        value={ colunsFilter }
+        data-testid="column-filter"
+        onChange={ handleFilterByColuns }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+
+      <select
+        value={ operatorFilter }
+        data-testid="comparison-filter"
+        onChange={ handleFilterByOperator }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+
+      <input
+        data-testid="value-filter"
+        type="number"
+        name="numberFilter"
+        value={ numberFilter }
+        placeholder="Digite um nome"
+        onChange={ handleFilterByNumber }
+      />
+
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleFilter }
+      >
+        Filtrar
+      </button>
+
     </div>
   );
 }
--- src/context/PlanetsProvider.jsx
+++ src/context/PlanetsProvider.jsx
@@ -5,15 +5,43 @@ import PlanetsContext from './PlanetsContext';
 function PlanetsProvider({ children }) {
   const [planets, setPlanets] = useState([]);
   const [nameFilter, setNameFilter] = useState('');
+  const [colunsFilter, setColunsFilter] = useState('population');
+  const [operatorFilter, setOperadorFilter] = useState('maior que');
+  const [numberFilter, setNumberFilter] = useState(0);
 
   const handleFilterByName = ({ target }) => setNameFilter(target.value);
+  const handleFilterByColuns = ({ target }) => setColunsFilter(target.value);
+  const handleFilterByOperator = ({ target }) => setOperadorFilter(target.value);
+  const handleFilterByNumber = ({ target }) => setNumberFilter(target.value);
+
+  const handleFilter = () => {
+    let planetsFiltered;
+    if (operatorFilter === 'maior que') {
+      planetsFiltered = planets
+        .filter((element) => Number(element[colunsFilter]) > Number(numberFilter));
+    } else if (operatorFilter === 'menor que') {
+      planetsFiltered = planets
+        .filter((element) => Number(element[colunsFilter]) < Number(numberFilter));
+    } else {
+      planetsFiltered = planets
+        .filter((element) => Number(element[colunsFilter]) === Number(numberFilter));
+    }
+    setPlanets(planetsFiltered);
+  };
 
   const value = useMemo(() => ({
     planets,
     setPlanets,
     nameFilter,
+    colunsFilter,
+    operatorFilter,
+    numberFilter,
     handleFilterByName,
-  }), [planets, nameFilter]);
+    handleFilterByColuns,
+    handleFilterByOperator,
+    handleFilterByNumber,
+    handleFilter,
+  }), [planets, nameFilter, colunsFilter, operatorFilter, numberFilter]);
 
   return (
     <PlanetsContext.Provider value={ value }>
